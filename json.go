a1690badb7e0a36ce7d7580c740e124f4d88300d
AKASH KASHU <kashthus@gmail.com>
2021-08-20 21:22:54 +0530

    update

--- json.go
+++ json.go
@@ -50,17 +50,21 @@ frontend nonssl
 	}
 	for _, frontend := range obj.Sites {
 		if frontend.PrimaryDomain.Routing == "www" {
-			conf = conf + fmt.Sprintf(`	redirect prefix http://www.%s code 301 if { hdr(host) -i %s }`, frontend.PrimaryDomain.Url, frontend.PrimaryDomain.Url)
+			conf = conf + fmt.Sprintf(`
+	redirect prefix http://www.%s code 301 if { hdr(host) -i %s }`, frontend.PrimaryDomain.Url, frontend.PrimaryDomain.Url)
 		}
 		if frontend.PrimaryDomain.Routing == "root" {
-			conf = conf + fmt.Sprintf(`	redirect prefix http://%s code 301 if { hdr(host) -i www.%s }`, frontend.PrimaryDomain.Url, frontend.PrimaryDomain.Url)
+			conf = conf + fmt.Sprintf(`	
+	redirect prefix http://%s code 301 if { hdr(host) -i www.%s }`, frontend.PrimaryDomain.Url, frontend.PrimaryDomain.Url)
 		}
 		for _, alias := range frontend.AliasDomain {
 			if alias.Routing == "www" {
-				conf = conf + fmt.Sprintf(` redirect prefix http://www.%s code 301 if { hdr(host) -i %s }`, alias.Url, alias.Url)
+				conf = conf + fmt.Sprintf(` 
+	redirect prefix http://www.%s code 301 if { hdr(host) -i %s }`, alias.Url, alias.Url)
 			}
 			if alias.Routing == "root" {
-				conf = conf + fmt.Sprintf(` redirect prefix http://%s code 301 if { hdr(host) -i www.%s }`, alias.Url, alias.Url)
+				conf = conf + fmt.Sprintf(`
+	redirect prefix http://%s code 301 if { hdr(host) -i www.%s }`, alias.Url, alias.Url)
 			}
 		}
 	}
