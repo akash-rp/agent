96d72261f883f1ec70351b322bed803f1a31b946
AKASH KASHU <kashthus@gmail.com>
2021-12-04 15:15:09 +0530

    update;

--- main/main.go
+++ main/main.go
@@ -45,7 +45,7 @@ func main() {
 	e.GET("/restorelocalbackup/:name/:user/:mode/:id/:type", restoreBackupFromPanel)
 	e.GET("/createstaging/:name/:user/:url/:livesiteurl", createStaging)
 	e.GET("/getdbtables/:name/:user", getDatabaseTables)
-	e.POST("/pushChanges", syncChanges)
+	e.POST("/syncChanges", syncChanges)
 	e.Logger.Fatal(e.Start(":8081"))
 }
 
